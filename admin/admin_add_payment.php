caf54163e85cedaaac1b5a880a3d4e8a53cdf482
leooncole-pixel <leooncole@gmail.com>
2026-06-25 18:56:14 +0800

    hi

--- admin/admin_add_payment.php
+++ admin/admin_add_payment.php
@@ -8,7 +8,6 @@ include '../database/database_payment.php';
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/admin_sidebar_topbar_searchbar_profile_icon.css">
-    <link rel="stylesheet" href="../admin/admin_payment.php">
     <link rel="stylesheet" href="../css/admin_add_payment.css">
     <title>MITZTIANPC WIRED INTERNET SERVICES</title>
 </head>
