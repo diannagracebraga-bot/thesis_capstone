574485757faf8fbb40a4c41c2745b92ff5334ff0
heartjoyabainza <heartjoy.abainza@gmail.com>
2026-07-09 17:49:54 +0800

    changesssss

--- customer/customer_profile.php
+++ customer/customer_profile.php
@@ -1,3 +1,32 @@
+<?php
+include '../database/database_connection.php';
+
+$customer_id = 1;
+$message = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $address = mysqli_real_escape_string($conn, $_POST["address"]);
+    $contact_number = mysqli_real_escape_string($conn, $_POST["contact_number"]);
+    $civil_status = mysqli_real_escape_string($conn, $_POST["civil_status"]);
+
+    $update = "UPDATE customer_tbl 
+               SET address='$address',
+                   contact_number='$contact_number',
+                   civil_status='$civil_status'
+               WHERE customer_id=$customer_id";
+
+    if (mysqli_query($conn, $update)) {
+        $message = "Profile updated successfully.";
+    } else {
+        $message = "Update failed.";
+    }
+}
+
+$query = "SELECT * FROM customer_tbl WHERE customer_id = $customer_id";
+$result = mysqli_query($conn, $query);
+$customer = mysqli_fetch_assoc($result);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,61 +44,63 @@
         <section class="profile-section">
             <h2>Customer Information</h2>
 
-            <form class="profile-form">
+           <form class="profile-form" method="POST">
                 <div class="form-grid">
                     <div class="field-group">
                         <label for="first_name">First Name:</label>
-                        <input id="first_name" type="text" value="Dianna" readonly>
+                        <input id="first_name" type="text" value="<?php echo $customer['first_name']; ?>" readonly>
                     </div>
 
                     <div class="field-group">
                         <label for="middle_name">Middle Name:</label>
-                        <input id="middle_name" type="text" value="Bautista" readonly>
+                        <input id="middle_name" type="text" value="<?php echo $customer['middle_name']; ?>" readonly>
+
                     </div>
 
                     <div class="field-group">
                         <label for="last_name">Last Name:</label>
-                        <input id="last_name" type="text" value="Braga" readonly>
+                        <input id="last_name" type="text" value="<?php echo $customer['last_name']; ?>" readonly>
                     </div>
 
                     <div class="field-group">
                         <label for="birth_date">Birth Date:</label>
-                        <input id="birth_date" type="text" value="7/20/2006" readonly>
+                       <input id="birth_date" type="text" value="<?php echo $customer['birth_date']; ?>" readonly>
                     </div>
 
                     <div class="field-group">
                         <label for="age">Age:</label>
-                        <input id="age" type="text" value="21" readonly>
+                        <input id="age" type="text" value="<?php echo $customer['age']; ?>" readonly>
+
                     </div>
 
                     <div class="field-group">
                         <label for="sex">Sex:</label>
-                        <input id="sex" type="text" value="Female" readonly>
+                        <input id="sex" type="text" value="<?php echo $customer['sex']; ?>" readonly>
                     </div>
 
                     <div class="field-group">
                         <label for="address">Address:</label>
-                        <input id="address" type="text" value="Tanza, Cavite">
+                        <input id="address" name="address" type="text" value="<?php echo $customer['address']; ?>">
                     </div>
 
                     <div class="field-group">
                         <label for="contact_number">Contact Number:</label>
-                        <input id="contact_number" type="text" value="09266468174">
+                        <input id="contact_number" name="contact_number" type="text" value="<?php echo $customer['contact_number']; ?>">
                     </div>
 
                     <div class="field-group">
                         <label for="civil_status">Civil Status:</label>
-                        <select id="civil_status">
-                            <option selected>Single</option>
-                            <option>Married</option>
-                            <option>Widowed</option>
-                            <option>Separated</option>
+                        <select id="civil_status" name="civil_status">
+                     <option <?php if ($customer['civil_status'] == 'Single') echo 'selected'; ?>>Single</option>
+                     <option <?php if ($customer['civil_status'] == 'Married') echo 'selected'; ?>>Married</option>
+                     <option <?php if ($customer['civil_status'] == 'Widowed') echo 'selected'; ?>>Widowed</option>
+                     <option <?php if ($customer['civil_status'] == 'Separated') echo 'selected'; ?>>Separated</option>
                         </select>
                     </div>
 
                     <div class="field-group">
                         <label for="internet_plan">Internet Plan:</label>
-                        <input id="internet_plan" type="text" value="800 php" readonly>
+                        <input id="internet_plan" type="text" value="<?php echo $customer['internet_plan_id']; ?>" readonly>
                     </div>
                 </div>
 
